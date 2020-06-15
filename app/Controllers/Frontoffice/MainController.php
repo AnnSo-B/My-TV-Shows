a51f73022a5c67adf5f34be2f0355ec6cb2b54c3
AnnSo-B <as.baujard.33@gmail.com>
2020-06-15 20:28:39 +0200

    add homepage controller + temporary view

--- app/Controllers/Frontoffice/MainController.php
+++ app/Controllers/Frontoffice/MainController.php
@@ -7,6 +7,21 @@ use App\Controllers\CoreController;
 class MainController extends CoreController {
 
     /**
+     * Method to display the frontoffice homepage
+     *
+     * @return void
+     */
+    public function home() {  
+        $this->show(
+            'frontoffice',
+            'main/home',
+            [
+                'headTitle' => 'Bienvenue sur '
+            ]
+        );
+    }
+
+    /**
      * Method to display the 404 page
      *
      * @return void
