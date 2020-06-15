0d8a370ecfcf0e53d61a6aa29f2eb2432eb99714
AnnSo-B <as.baujard.33@gmail.com>
2020-06-15 14:31:06 +0200

    check user's authorization to access to a page

--- app/Controllers/CoreController.php
+++ app/Controllers/CoreController.php
@@ -3,6 +3,30 @@
 namespace App\Controllers;
 
 class CoreController {
+
+    /**
+     * Method launched every time a new object is instanciated
+     */
+    public function __construct()
+    {
+        //* check the authorization for the current page
+        // we need $match variable which contains the page name
+        global $match;
+
+        // get the route name
+        $currentRoute = $match['name'];
+
+        // include the access control list
+        require __DIR__ . '../../Routes/acl.php';
+
+        // if the route is in the access control list, we'll want to check the user is authorized to see it
+        if (array_key_exists($currentRoute, $controlList)) {
+            // checkAuthorization needs the list as argument
+            $this->checkAuthorization($controlList[$currentRoute]);
+        }
+    }
+
+
    /**
      * Method to display the views
      *
@@ -16,12 +40,12 @@ class CoreController {
         // we need the router
         global $router;
         
-        // Data to be sent to every single page
+        //* Data to be sent to every single page
         $viewVars['currentPage'] = $viewName; 
         $viewVars['assetsBaseUri'] = $_SERVER['BASE_URI'] . 'assets/';
         $viewVars['baseUri'] = $_SERVER['BASE_URI'];
 
-        // if we have messages in session, 
+        //* if we have messages in session, 
         if (isset($_SESSION['sessionMessages'])) {
             // we'll want to display them so we send them to the view
             $viewVars['sessionMessages'] = $_SESSION['sessionMessages'];
@@ -32,7 +56,7 @@ class CoreController {
             $viewVars['sessionMessages'] = [];
         }
 
-        // if we have formData in session, 
+        //* if we have formData in session, 
         if (isset($_SESSION['formData'])) {
             // we'll want to display them so we send them to the view
             $viewVars['formData'] = $_SESSION['formData'];
@@ -48,13 +72,43 @@ class CoreController {
             $viewVars['formData']['status'] = 0;
         }
 
+        //* extract transforms array keys into variables
         // https://www.php.net/manual/fr/function.extract.php
-        // extract transforms array keys into variables
         extract($viewVars);
 
-        // the different templates that will compose the view
+        //* the different templates that will compose the view
         require_once __DIR__.'/../Views/' . $office . '/layout/header.tpl.php';
         require_once __DIR__.'/../Views/' . $office . '/' .$viewName.'.tpl.php';
         require_once __DIR__.'/../Views/' . $office . '/layout/footer.tpl.php';
+    }
+
+    /**
+     * Method to check user authorization
+     * 
+     * @param $accessControlList
+     * @return bool either the user is authorized to display the current page
+     */
+    protected function checkAuthorization($roles = [])
+    {
+        // we need the router
+        global $router;
+
+        // if the user is connected, we can find his data in session
+        if (array_key_exists('userId', $_SESSION) && array_key_exists('userData', $_SESSION)) {
+            // if the user role is included in the authorized roles
+            if (in_array($_SESSION['userData']->getRoleId(), $roles)) {
+                // he is authorized to continue on this page
+                return true;
+            }
+            else {
+                // we want to redirect the user to the 403 error page
+                // TODO Create 403 error page
+            }
+        }
+        // if the user is not connected
+        else {
+            // redirect to the login form
+            header('Location: ' . $router->generate('frontoffice-user-login'));
+        }
     }
 }
