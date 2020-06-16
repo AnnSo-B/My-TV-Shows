d768f4a8c0512c91845f2ff7e1c5ceacaa2f129b
AnnSo-B <as.baujard.33@gmail.com>
2020-06-16 20:06:08 +0200

    improve selection form

--- app/Views/backoffice/category/selection.tpl.php
+++ app/Views/backoffice/category/selection.tpl.php
@@ -1,3 +1,7 @@
+<?php
+dump($formData['homeCategorySelection']);
+?>
+
 <main class="container my-4">
   <a class="btn btn-success float-right" href="<?= $router->generate('backoffice-main-home') ?>">Retour</a>
   <h2>Sélection des catégories affichées sur la page d'accueil</h2>
@@ -7,90 +11,49 @@
   <form action="<?= $router->generate('backoffice-category-selection-post') ?>" method="POST">
     <input type="hidden" id="csrfToken" name="csrfToken" value="<?= $csrfToken ?>"/>
 
+    <!-- First and second category are on the same row -->
     <div class="row">
-      <div class="col">
-        <div class="form-group">
-          <label for="home-order-1">1ère catégorie</label>
-          <select class="form-control" id="home-order-1" name="home_order[]">
-            <option value="">A sélectionner</option>
-            <?php foreach ($list as $elem) : ?>
-              <option
-                value="<?= $elem->getId() ?>"
-                <?= $elem->getHomeOrder() == 1 ? 'selected' : '' ?>
-              >
-                <?= $elem->getName() ?>
-              </option>
-            <?php endforeach ?>
-          </select>
+      <?php for ($index = 1; $index < 3; $index++) : ?>
+        <div class="col">
+          <div class="form-group">
+            <label for="home-order-<?= $index ?>">Catégorie #<?= $index ?></label>
+            <select class="form-control" id="home-order-<?= $index ?>" name="home_order[]">
+              <option value="">A sélectionner</option>
+              <?php foreach ($list as $elem) : ?>
+                <option
+                  value="<?= $elem->getId() ?>"
+                  <?= $elem->getHomeOrder() == $index ? 'selected' : '' ?>
+                >
+                  <?= $elem->getName() ?>
+                </option>
+              <?php endforeach ?>
+            </select>
+          </div>
         </div>
-      </div>
-      <div class="col">
-        <div class="form-group">
-          <label for="home-order-2">2nde catégorie</label>
-          <select class="form-control" id="home-order-2" name="home_order[]">
-            <option value="">A sélectionner</option>
-            <?php foreach ($list as $elem) : ?>
-              <option
-                value="<?= $elem->getId() ?>"
-                <?= $elem->getHomeOrder() == 2 ? 'selected' : '' ?>
-              >
-                <?= $elem->getName() ?>
-              </option>
-            <?php endforeach ?>
-          </select>
-        </div>
-      </div>
+      <?php endfor ?>
     </div>
 
+    <!-- Third, fourth and fifth category are on the same row -->
     <div class="row">
-      <div class="col">
-        <div class="form-group">
-          <label for="home-order-3">3ème catégorie</label>
-          <select class="form-control" id="home-order-3" name="home_order[]">
-            <option value="">A sélectionner</option>
-            <?php foreach ($list as $elem) : ?>
-              <option
-                value="<?= $elem->getId() ?>"
-                <?= $elem->getHomeOrder() == 3 ? 'selected' : '' ?>
-              >
-                <?= $elem->getName() ?>
-              </option>
-            <?php endforeach ?>
-          </select>
+      <?php for ($index = 3; $index < 6; $index++) : ?>
+        <div class="col">
+          <div class="form-group">
+            <label for="home-order-<?= $index ?>">Catégorie #<?= $index ?></label>
+            <select class="form-control" id="home-order-<?= $index ?>" name="home_order[]">
+              <option value="">A sélectionner</option>
+              <?php foreach ($list as $elem) : ?>
+                <option
+                  value="<?= $elem->getId() ?>"
+                  <?= $elem->getHomeOrder() == $index ? 'selected' : '' ?>
+                >
+                  <?= $elem->getName() ?>
+                </option>
+              <?php endforeach ?>
+            </select>
+          </div>
         </div>
-      </div>
-      <div class="col">
-        <div class="form-group">
-          <label for="home-order-4">4ème catégorie</label>
-          <select class="form-control" id="home-order-4" name="home_order[]">
-            <option value="">A sélectionner</option>
-            <?php foreach ($list as $elem) : ?>
-              <option
-                value="<?= $elem->getId() ?>"
-                <?= $elem->getHomeOrder() == 4 ? 'selected' : '' ?>
-              >
-                <?= $elem->getName() ?>
-              </option>
-            <?php endforeach ?>
-          </select>
-        </div>
-      </div>
-      <div class="col">
-        <div class="form-group">
-          <label for="home-order-5">5ème catégorie</label>
-          <select class="form-control" id="home-order-5" name="home_order[]">
-            <option value="">A sélectionner</option>
-            <?php foreach ($list as $elem) : ?>
-              <option
-                value="<?= $elem->getId() ?>"
-                <?= $elem->getHomeOrder() == 5 ? 'selected' : '' ?>
-              >
-                <?= $elem->getName() ?>
-              </option>
-            <?php endforeach ?>
-          </select>
-        </div>
-      </div>
+      <?php endfor ?>
+    
     </div>
     <button type="submit" class="btn btn-primary">Envoyer</button>
   </form>
