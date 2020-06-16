de124dbe2198980ef1d9b95c397adee60dbe2868
AnnSo-B <as.baujard.33@gmail.com>
2020-06-16 16:41:28 +0200

    add route to category selection page

--- app/Routes/Backoffice/category.php
+++ app/Routes/Backoffice/category.php
@@ -97,3 +97,16 @@ $router->map(
   ],
   'backoffice-category-delete-post'
 );
+
+/**
+ * Route to select the 5 categories displayed in frontoffice homepage
+ */
+$router->map(
+  'GET',
+  '/backoffice/category/selection',
+  [
+      'method' => 'selection',
+      'controller' => '\App\Controllers\Backoffice\CategoryController'
+  ],
+  'backoffice-category-selection'
+);
